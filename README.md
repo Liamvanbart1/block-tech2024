c78f4b7d29cef8ce012af49a4731749c88d0b857
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2024-02-16 14:24:27 +0100

    Les verdieping backend

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # block-tech2024
+
+npm run dev om de server te starten.
+
+ESJ of EJS is een template engine die wordt gebruikt om HTML te genereren met JavaScript.
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.get('/', function (req, res) {
-  res.send('Deze server runt goed loopt op poort 3000!')
+// Middleware
+
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+
+// Routes 
+
+app.get('/', (req, res) => {
+  res.render('base');
 })
 
-app.listen(3000)
+app.get('/register', (req, res) => {
+  res.send('Registratiepagina');
+});
 
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 
